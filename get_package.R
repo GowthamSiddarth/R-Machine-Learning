ba7ac94e37aa0d8069fff56158a3a8915937b236
Gowtham Behara <gothsiddu@gmail.com>
2018-05-19 16:26:29 +0530

    Install and load packages if not found

---
+++ get_package.R
@@ -0,0 +1,9 @@
+get_package <- function(pckg) {
+  if (!is.element(pckg, installed.packages()[, 1])) {
+    print(paste(pckg, "not found in the installed packages"))
+    print("Installing now...")
+    install.packages(pckg, dependencies = TRUE)
+  }
+  print(paste("loading", pckg, "into workspace"))
+  library(pckg, character.only = TRUE)
+}
