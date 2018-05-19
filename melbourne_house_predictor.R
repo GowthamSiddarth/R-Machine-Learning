5fb7e00061a320c7c674a1fbe5e2c437c9eee0c9
Gowtham Behara <gothsiddu@gmail.com>
2018-05-19 17:05:14 +0530

    Train and fit melb_data using regression tree

--- melbourne_house_predictor.R
+++ melbourne_house_predictor.R
@@ -6,9 +6,23 @@ cat("\014")
 
 source('get_package.R')
 
+# Install and/or load packages
 get_package('tidyverse')
 get_package('rpart')
 get_package('randomForest')
 
+# Read data into workspace
 melb_data <- read.csv('data/melb_data.csv')
+
+# Summarize the data
 summary(melb_data)
+
+# Train the data with features and output
+fit <- rpart(Price ~ Rooms + Bathroom + Landsize + BuildingArea + YearBuilt + Lattitude 
+             + Longtitude, data = melb_data)
+
+# Plot regression tree
+plot(fit, uniform = TRUE)
+
+# add text to regression tree
+text(fit, cex=0.6)
